8dede641a7f5626b6eb03e02ca6d0aee1fbc0fbb
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-07-15 17:12:15 +0100

    Added text effect to h3

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const typewriter = document.querySelector(".typewriter");
+const text = typewriter.textContent;
+let charIndex = 0;
+
+typewriter.textContent = "";
+
+setInterval(() => {
+  typewriter.textContent += text[charIndex];
+  charIndex++;
+  if (charIndex >= text.length) {
+    charIndex = 0;
+  }
+}, 100);
